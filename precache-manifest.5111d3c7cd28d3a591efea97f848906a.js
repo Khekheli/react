23a8c87e3b3414f33843fba9468e812c159deee2
unknown <denniskotoku@gmail.com>
2020-08-12 12:05:17 +0000

    Updates

---
+++ precache-manifest.5111d3c7cd28d3a591efea97f848906a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94202f1e475139ff42d1f2adc883b712",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "3820e88bdf797a948e2f",
+    "url": "/react/static/css/main.fa683e8d.chunk.css"
+  },
+  {
+    "revision": "0b7865d947e109b9c8ae",
+    "url": "/react/static/js/2.3fb98c22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.3fb98c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3820e88bdf797a948e2f",
+    "url": "/react/static/js/main.47670e40.chunk.js"
+  },
+  {
+    "revision": "1261bb7dddc870d8b4f4",
+    "url": "/react/static/js/runtime-main.5faf2419.js"
+  },
+  {
+    "revision": "704344044cbf925ca6dd07c8d69348f3",
+    "url": "/react/static/media/esposito.70434404.png"
+  },
+  {
+    "revision": "0a06f7bf7041234f1b0b3b78cacc94ab",
+    "url": "/react/static/media/ianis.0a06f7bf.png"
+  },
+  {
+    "revision": "091a28115d606dbf7090f487b76d7354",
+    "url": "/react/static/media/ihattaren.091a2811.png"
+  },
+  {
+    "revision": "701f4dc00f44dd9f9bbd5400d1bf401a",
+    "url": "/react/static/media/jude.701f4dc0.png"
+  },
+  {
+    "revision": "c8ba047f27a30298678d1c29e737b1cb",
+    "url": "/react/static/media/kai.c8ba047f.png"
+  },
+  {
+    "revision": "b19e223f90311801c1bcb482ec9ad12a",
+    "url": "/react/static/media/marin.b19e223f.png"
+  },
+  {
+    "revision": "b94c6b6ecdc27c715718c76164939b29",
+    "url": "/react/static/media/pinamonti.b94c6b6e.png"
+  },
+  {
+    "revision": "d4fe415c0d6b3148d11034aec95ce376",
+    "url": "/react/static/media/rodrygo.d4fe415c.png"
+  },
+  {
+    "revision": "5ee8babefcc4ecd4fa726181a07d17a6",
+    "url": "/react/static/media/zaracho.5ee8babe.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.7e48ffb6cc45953a5d111f75436665f8.js"
+  "/react/precache-manifest.5111d3c7cd28d3a591efea97f848906a.js"
 );
 
 self.addEventListener('message', (event) => {
