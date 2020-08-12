f0e1609384b7b03acbf7eeb0a7e18c823e490bd7
unknown <denniskotoku@gmail.com>
2020-08-12 12:03:32 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-import pic34 from "./images/Group 2.png";
-import pic22 from "./images/Group 2.png";
-import pic56 from "./images/Group 2.png";
+import pic1 from "./images/kai.png";
+import pic2 from "./images/rodrygo.png";
+import pic3 from "./images/ihattaren.png";
+import pic4 from "./images/esposito.png";
+import pic5 from "./images/jude.png";
+import pic6 from "./images/zaracho.png";
+import pic7 from "./images/marin.png";
+import pic8 from "./images/pinamonti.png";
+import pic9 from "./images/ianis.png";
 
 //adding props
 const Person = ({ img, name, job, children }) => {
@@ -23,18 +29,86 @@ const Person = ({ img, name, job, children }) => {
 //now to create a component
 const PersonList = () => {
   return (
-    <section className="person-list">
-      <Person img="34" name="Khekheli" job="Web Developer">
-        <img src={pic34}></img>
-      </Person>
-      <Person img="22" name="Enam" job="Personal Trainer">
-        <img src={pic22}></img>
-      </Person>
-      <Person img="56" name="Dennis" job="Accountant">
-        <img src={pic56}></img>
-        <p>lorem</p>
-      </Person>
-    </section>
+    <div>
+      <section className="person-list">
+        <Person img="" name="Kai Havertz" job="FM Wonderkid">
+          <img src={pic1} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+        <Person img="" name="Rodrygo" job="FM Wonderkid">
+          <img src={pic2} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+        <Person img="" name="Mohammed Ihattaren" job="FM Wonderkid">
+          <img src={pic3} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+      </section>
+      <section className="person-list">
+        <Person img="" name="Sebastiano Esposito" job="FM Wonderkid">
+          <img src={pic4} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+        <Person img="" name="Jude Bellingham" job="Personal Trainer">
+          <img src={pic5} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+        <Person img="" name="Matias Zaracho" job="Accountant">
+          <img src={pic6} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+      </section>
+      <section className="person-list">
+        <Person img="" name="Antonio Marin" job="Web Developer">
+          <img src={pic7} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+        <Person img="" name="Andrea Pinamonti" job="FM Wonderkid">
+          <img src={pic8} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+        <Person img="" name="Ianis Hagi" job="Accountant">
+          <img src={pic9} alt=""></img>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            consectetur nemo sint iure, animi eum deleniti ducimus totam, at
+            repudiandae blanditiis laboriosam facere eaque maiores?
+          </p>
+        </Person>
+      </section>
+    </div>
   );
 };
 
